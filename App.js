b56b40819e0307f5b61ecd3abdfb71d20b2909ba
SyotaKawakami <49909868+syotaKawakami@users.noreply.github.com>
2019-12-23 01:31:56 +0900

    Style MemoDatailScreen

--- App.js
+++ App.js
@@ -3,13 +3,15 @@ import { StyleSheet, View } from 'react-native';
 
 import Appbar from './src/components/Appbar';
 import MemoListScreen from './src/screens/MemoListScreen';
+import MemoDetailScreen from './src/screens/MemoDetailScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
 
       <Appbar></Appbar>
-      <MemoListScreen></MemoListScreen>
+      {/* <MemoListScreen></MemoListScreen> */}
+      <MemoDetailScreen></MemoDetailScreen>
     </View>
   );
 }
