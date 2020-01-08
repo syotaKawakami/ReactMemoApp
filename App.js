6cbdc7bf062b0dd8e867682399554ba69dc8b0cb
SyotaKawakami <49909868+syotaKawakami@users.noreply.github.com>
2020-01-08 23:25:28 +0900

    react-navigation導入

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack';
 
 import Appbar from './src/components/Appbar';
 import MemoListScreen from './src/screens/MemoListScreen';
@@ -7,25 +9,31 @@ import MemoDetailScreen from './src/screens/MemoDetailScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-
-      <Appbar></Appbar>
-      {/* <MemoListScreen></MemoListScreen> */}
-      {/* <MemoDetailScreen></MemoDetailScreen> */}
-      {/* <LoginScreen></LoginScreen> */}
-      <SignupScreen></SignupScreen>
-    </View>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+      </View>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fffdf6',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 78,
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: MemoListScreen
   },
-});
+})
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fffdf6',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     paddingTop: 78,
+//   },
+// });
+
+export default createAppContainer(AppNavigator);
